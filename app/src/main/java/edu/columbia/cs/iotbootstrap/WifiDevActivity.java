ae5b4a42fb32a7307606a11aea53d34b11edbd8d
Lianghua Xu <lx2180@columbia.edu>
2016-02-23 22:36:11 -0500

    cleaned up and update java version

--- app/src/main/java/edu/columbia/cs/iotbootstrap/WifiDevActivity.java
+++ app/src/main/java/edu/columbia/cs/iotbootstrap/WifiDevActivity.java
@@ -2,10 +2,7 @@ package edu.columbia.cs.iotbootstrap;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.wifi.p2p.WifiP2pDevice;
-import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
