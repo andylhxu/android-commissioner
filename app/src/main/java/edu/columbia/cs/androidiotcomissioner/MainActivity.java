c21026bc722d19def7d1b4a94af15c2d45b14b15
Andy Lianghua Xu <lx2180@columbia.edu>
2016-05-22 17:28:11 -0400

    basic layout

--- app/src/main/java/edu/columbia/cs/androidiotcomissioner/MainActivity.java
+++ app/src/main/java/edu/columbia/cs/androidiotcomissioner/MainActivity.java
@@ -1,8 +1,6 @@
 package edu.columbia.cs.androidiotcomissioner;
 
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -12,13 +10,8 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
